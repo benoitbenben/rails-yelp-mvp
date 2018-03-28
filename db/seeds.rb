3e8851f82860600667df4350914bc9691510be5a
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-28 16:26:37 +0200

    controlleur restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
 Restaurant.destroy_all
 
 10.times do
-  restaurant =Restaurant.new(
+  restaurant = Restaurant.new(
     name: Faker::Name.name,
     address: ['dolphin', 'zebu', 'snoop', 'meerkat'].sample,
     phone_number: Faker::Number.number(10),
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
     )
-end
-restaurant.save!
 
-puts 'resto added!'
+  restaurant.save!
 
-
-5.times do
-  review = Review.new(
-    content: Faker::Name.name,
-    rating: (0..5).sample,
- )
-   review.restaurant_id = restaurant.id
-    review.save!
+  5.times do
+    review = Review.new(
+      content: Faker::Name.name,
+      rating: (0..5).to_a.sample
+  )
+     review.restaurant_id = restaurant.id
+      review.save!
   end
-puts 'reviews added!'
-
-
-
-
+end
+puts 'database... validate'
 
