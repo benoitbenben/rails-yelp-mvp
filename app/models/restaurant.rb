7ce9ac72597b9af512e109c08f2ada6cb9124eab
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-28 13:51:16 +0200

    create models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,4 @@
+class Restaurant < ApplicationRecord
+
+    validate: has_many_reviews
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180328104410) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.string "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
